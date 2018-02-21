d73d6728f11fe0c44502e79d9dfee8410a3f8cef
roquealex <roquealex20@hotmail.com>
2018-02-20 23:46:40 -0800

    Adding KNN script

---
+++ DBDA.X414.2/Assignment1/KNN.r
@@ -0,0 +1,127 @@
+###Classification using Nearest Neighbors --------------------
+
+## Example: Classifying Cancer Samples ----
+rm(list = ls())
+## Step 2: Exploring and preparing the data ---- 
+
+# import the CSV file
+#wbcd <- read.csv("/Users/home/Desktop/R_SCRIPTS/wisc_bc_data.csv", stringsAsFactors = FALSE)
+wbcd <- read.csv("wisc_bc_data.csv", stringsAsFactors = FALSE)
+
+# examine the structure of the wbcd data frame
+str(wbcd)
+
+# drop the id feature as it does not provide any information
+wbcd <- wbcd[,-1]
+
+#sum(is.na(wbcd$diagnosis))
+
+# table of diagnosis
+table(wbcd$diagnosis)
+
+# recode diagnosis as a factor
+wbcd$diagnosis <- factor(wbcd$diagnosis, levels = c("B", "M"),labels = c("Benign", "Malignant"))
+
+# table or proportions with more informative labels
+round(prop.table(table(wbcd$diagnosis)) * 100, digits = 1)
+
+# summarize the data to get the idea baout various features
+summary(wbcd)
+
+# it seems that we need to normalize the data
+
+# create normalization function
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+# normalize the wbcd data
+wbcd_n <- as.data.frame(lapply(wbcd[2:31], normalize))
+
+# confirm that normalization worked
+summary(wbcd_n$area_mean)
+
+# create training and test data usually 70% and 30%
+wbcd_train <- wbcd_n[1:469, ]
+wbcd_test <- wbcd_n[470:569, ]
+
+# create labels for training and test data
+
+wbcd_train_labels <- wbcd[1:469, 1]
+wbcd_test_labels <- wbcd[470:569, 1]
+
+## Step 3: Training a model on the data ----
+# How to to choose the number of K 
+# Thumb rule is usually sqrt of number of 
+# training samples
+
+sqrt(nrow(wbcd_train))
+
+# load the "class" library
+#library(class)
+if(!require(FNN)) install.packages("FNN")
+library(FNN)
+wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test,
+                      cl = wbcd_train_labels, k=21)
+
+
+## Step 4: Evaluating model performance ----
+table(wbcd_test_labels, wbcd_test_pred)
+prop.table(table(wbcd_test_labels, wbcd_test_pred))
+
+sum(wbcd_test_pred == wbcd_test_labels)/length(wbcd_test_labels)
+
+# load the "gmodels" library
+if(!require(gmodels))install.packages("gmodels")
+library(gmodels)
+
+# Create the cross tabulation of predicted vs. actual
+CrossTable(x = wbcd_test_labels, y = wbcd_test_pred,
+           prop.chisq=FALSE)
+
+# To check the nearest neighbors
+indices <-attr(wbcd_test_pred, "nn.index")
+# To see the nearest neighbors of row 10
+indices[10,]
+
+## Step 5: Improving model performance ----
+
+# use the scale() function to z-score standardize a data frame
+wbcd_z <- as.data.frame(scale(wbcd[-1]))
+
+# confirm that the transformation was applied correctly
+summary(wbcd_z$area_mean)
+
+# create training and test datasets
+wbcd_train <- wbcd_z[1:469, ]
+wbcd_test <- wbcd_z[470:569, ]
+
+# re-classify test cases
+wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test,
+                      cl = wbcd_train_labels, k=21)
+
+# Create the cross tabulation of predicted vs. actual
+CrossTable(x = wbcd_test_labels, y = wbcd_test_pred,
+           prop.chisq=FALSE)
+
+# try several different values of k
+wbcd_train <- wbcd_n[1:469, ]
+wbcd_test <- wbcd_n[470:569, ]
+
+wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test, cl = wbcd_train_labels, k=1)
+CrossTable(x = wbcd_test_labels, y = wbcd_test_pred, prop.chisq=FALSE)
+
+wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test, cl = wbcd_train_labels, k=5)
+CrossTable(x = wbcd_test_labels, y = wbcd_test_pred, prop.chisq=FALSE)
+
+wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test, cl = wbcd_train_labels, k=11)
+CrossTable(x = wbcd_test_labels, y = wbcd_test_pred, prop.chisq=FALSE)
+
+wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test, cl = wbcd_train_labels, k=15)
+CrossTable(x = wbcd_test_labels, y = wbcd_test_pred, prop.chisq=FALSE)
+
+wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test, cl = wbcd_train_labels, k=21)
+CrossTable(x = wbcd_test_labels, y = wbcd_test_pred, prop.chisq=FALSE)
+
+wbcd_test_pred <- knn(train = wbcd_train, test = wbcd_test, cl = wbcd_train_labels, k=27)
+CrossTable(x = wbcd_test_labels, y = wbcd_test_pred, prop.chisq=FALSE)
