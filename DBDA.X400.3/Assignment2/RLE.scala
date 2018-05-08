b6b08927ef3a31e8678b7b9c7db0dda083ddea46
roquealex <roquealex20@hotmail.com>
2018-05-07 20:39:54 -0700

    Indentation fixed

--- DBDA.X400.3/Assignment2/RLE.scala
+++ DBDA.X400.3/Assignment2/RLE.scala
@@ -1,106 +1,106 @@
 object RLE {
 
-	def encodingIter(input:String) : String = {
-		var i:Int = 0
-		val buf = new StringBuilder
-		while(i < input.length) {
-			val c = input.charAt(i)
-			val idx = input.indexWhere(_!=c,i+1)
-			val lim = if (idx>0) idx else input.length
-			buf ++= (lim-i).toString
-			buf += c
-			i = lim
-		}
-		return(buf.toString)
-	}
+  def encodingIter(input:String) : String = {
+    var i:Int = 0
+    val buf = new StringBuilder
+    while(i < input.length) {
+      val c = input.charAt(i)
+      val idx = input.indexWhere(_!=c,i+1)
+      val lim = if (idx>0) idx else input.length
+      buf ++= (lim-i).toString
+      buf += c
+      i = lim
+    }
+    return(buf.toString)
+  }
 
-	def decodingIter(input:String) : String = {
-		val isNotNum = (c:Char) => !(c>= '0' && c <='9')
-		var i:Int = 0
-		val buff = new StringBuilder
-		while(i < input.length) {
-			val idx = input.indexWhere(isNotNum,i)
-			val lim = if (idx>=0) idx else input.length
-			try {
-				val rep = input.slice(i,lim).toInt
-				for (i <- 0 until rep) buff += input.charAt(lim)
-			} catch {
-				// There is a character without number "23XX"
-				case e : NumberFormatException => buff += input.charAt(lim)
-				// String ends in number "2X12" bypassing number
-				case e : StringIndexOutOfBoundsException => buff ++=  input.slice(i,lim)
-			}
-			i = lim+1
-		}
-		return(buff.toString)
-	}
+  def decodingIter(input:String) : String = {
+    val isNotNum = (c:Char) => !(c>= '0' && c <='9')
+    var i:Int = 0
+    val buff = new StringBuilder
+    while(i < input.length) {
+      val idx = input.indexWhere(isNotNum,i)
+      val lim = if (idx>=0) idx else input.length
+      try {
+        val rep = input.slice(i,lim).toInt
+          for (i <- 0 until rep) buff += input.charAt(lim)
+      } catch {
+        // There is a character without number "23XX"
+        case e : NumberFormatException => buff += input.charAt(lim)
+                 // String ends in number "2X12" bypassing number
+        case e : StringIndexOutOfBoundsException => buff ++=  input.slice(i,lim)
+      }
+      i = lim+1
+    }
+    return(buff.toString)
+  }
 
-	def encoding(input:String) : String = {
-		def charEncoding(cl:List[Char]) : String = cl match {
-			case Nil => return("")
-			case c::rest => {
-				val (pre,post) = rest.span(_==c)
-				return (s"${pre.length+1}$c" + charEncoding(post))
-			}
-		}
-		return(charEncoding(input.toList))
-	}
+  def encoding(input:String) : String = {
+    def charEncoding(cl:List[Char]) : String = cl match {
+      case Nil => return("")
+      case c::rest => {
+        val (pre,post) = rest.span(_==c)
+        return (s"${pre.length+1}$c" + charEncoding(post))
+      }
+    }
+    return(charEncoding(input.toList))
+  }
 
-	def decoding(input:String) : String = {
-		val isNum = (c:Char) => (c>= '0' && c <='9')
-		input match {
-			case x if (x.isEmpty) => return "";
-			// This is in case a letter comes alone let it pass:
-			case x if (!isNum(x.head)) => return (x.head)+decoding(x.tail)
-			case _ => {
-				val (num, rest) = input.span(isNum)
-				// can throw NumberFormatException
-				return (rest.head.toString*num.toInt + decoding(rest.tail))
-			}
-		}
-	}
+  def decoding(input:String) : String = {
+    val isNum = (c:Char) => (c>= '0' && c <='9')
+    input match {
+      case x if (x.isEmpty) => return "";
+      // This is in case a letter comes alone let it pass:
+      case x if (!isNum(x.head)) => return (x.head)+decoding(x.tail)
+      case _ => {
+        val (num, rest) = input.span(isNum)
+        // can throw NumberFormatException
+        return (rest.head.toString*num.toInt + decoding(rest.tail))
+      }
+    }
+  }
 
-	def main(arg : Array[String]) {
-		val tests = List(
-			("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWW","12W1B12W3B8W"),
-			("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW","12W1B12W3B24W1B14W"),
-			("aaabbcbbbccccdd","3a2b1c3b4c2d"),
-			("XXXXYYYXXYXYYYYYYYYYYYY","4X3Y2X1Y1X12Y"),
-			("ABCABCABCABC","1A1B1C1A1B1C1A1B1C1A1B1C"),
-			("CommonRunner","1C1o2m1o1n1R1u2n1e1r"),
-			("eeeeei","5e1i"),
-			("","")
-		)
-		println("RLE test")
-		for((raw,rle) <- tests) {
-			println(s"Testing Encoding $raw")
-			val enc = encoding(raw)
-			println(s"Expected: $rle, Calculated $enc")
-			println("Test Enc "+ (if (enc==rle) "PASSED" else "FAILED"))
-			val encIt = encodingIter(raw)
-			println(s"Expected (Iter): $rle, Calculated $encIt")
-			println("Test Enc "+ (if (encIt==rle) "PASSED" else "FAILED"))
-			println(s"Testing Decoding $rle")
-			val dec = decoding(rle)
-			println(s"Expected: $raw, Calculated $dec")
-			println("Test Dec "+ (if (dec==raw) "PASSED" else "FAILED"))
-			val decIt = decodingIter(rle)
-			println(s"Expected (Iter): $raw, Calculated $decIt")
-			println("Test Dec "+ (if (decIt==raw) "PASSED" else "FAILED"))
-		}
-		//encodingIter(tests(5)._1)
-		//encodingIter("")
-		//encodingIter("eeeeee")
-		//println(decodingIter("2eC4dX12Y"))
-		//println(decodingIter("aaaabaa"))
-	}
+  def main(arg : Array[String]) {
+    val tests = List(
+      ("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWW","12W1B12W3B8W"),
+      ("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW","12W1B12W3B24W1B14W"),
+      ("aaabbcbbbccccdd","3a2b1c3b4c2d"),
+      ("XXXXYYYXXYXYYYYYYYYYYYY","4X3Y2X1Y1X12Y"),
+      ("ABCABCABCABC","1A1B1C1A1B1C1A1B1C1A1B1C"),
+      ("CommonRunner","1C1o2m1o1n1R1u2n1e1r"),
+      ("eeeeei","5e1i"),
+      ("","")
+    )
+    println("RLE test")
+    for((raw,rle) <- tests) {
+      println(s"Testing Encoding $raw")
+      val enc = encoding(raw)
+      println(s"Expected: $rle, Calculated $enc")
+      println("Test Enc "+ (if (enc==rle) "PASSED" else "FAILED"))
+      val encIt = encodingIter(raw)
+      println(s"Expected (Iter): $rle, Calculated $encIt")
+      println("Test Enc "+ (if (encIt==rle) "PASSED" else "FAILED"))
+      println(s"Testing Decoding $rle")
+      val dec = decoding(rle)
+      println(s"Expected: $raw, Calculated $dec")
+      println("Test Dec "+ (if (dec==raw) "PASSED" else "FAILED"))
+      val decIt = decodingIter(rle)
+      println(s"Expected (Iter): $raw, Calculated $decIt")
+      println("Test Dec "+ (if (decIt==raw) "PASSED" else "FAILED"))
+    }
+    //encodingIter(tests(5)._1)
+    //encodingIter("")
+    //encodingIter("eeeeee")
+    //println(decodingIter("2eC4dX12Y"))
+    //println(decodingIter("aaaabaa"))
+  }
 }
 
 
 
 /*
 
-scala> val str = "AAAAABBBBBCCCD"
+   scala> val str = "AAAAABBBBBCCCD"
 str: String = AAAAABBBBBCCCD
 
 scala> str.span(_=='A')
@@ -120,7 +120,7 @@ scala>
 scala> s"${sp._1.length}${sp._1.head}"
 res4: String = 5A
 
-				//java.lang.NumberFormatException
-				//java.lang.StringIndexOutOfBoundsException
+//java.lang.NumberFormatException
+//java.lang.StringIndexOutOfBoundsException
 
-*/
+ */
