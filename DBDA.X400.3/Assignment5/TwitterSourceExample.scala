e2dcb46c0c786809f43c6aadb7a26a4fb121ba4e
roquealex <roquealex20@hotmail.com>
2018-06-20 09:19:43 -0700

    Ready before cleanup

--- DBDA.X400.3/Assignment5/TwitterSourceExample.scala
+++ DBDA.X400.3/Assignment5/TwitterSourceExample.scala
@@ -17,6 +17,17 @@ object TwitterSourceExample {
   def main(args: Array[String]): Unit = {
     println("TwitterSourceExample")
 
+    // This flag will print the tweet count to the terminal if true
+    val DEBUG_TWEET_COUNT = true
+    // This flag will write the output to csv files in directories
+    // src/main/output/csv30
+    // src/main/output/csv10
+    // and checkpoints:
+    // src/main/output/checkpoint30
+    // src/main/output/checkpoint10
+    // Deleted src/main/output/ before running
+    val SINK_REPORT_TO_CVS = true
+
     val providerClassName = SOURCE_PROVIDER_CLASS.substring(0, SOURCE_PROVIDER_CLASS.indexOf("$"))
     println(providerClassName)
 
@@ -34,6 +45,8 @@ object TwitterSourceExample {
       .appName("TwitterStructuredStreaming")
       .master("local[*]")
       .getOrCreate()
+
+    //import spark.implicits._
 
     println("Spark version: " + spark.version)
 
@@ -55,6 +68,7 @@ object TwitterSourceExample {
 
     tweetDF.printSchema()
 
+    /*
     val folder = "src/main/resources"
 
     val positive = s"$folder/pos-words.txt"
@@ -69,7 +83,6 @@ object TwitterSourceExample {
     stopDF.persist()
     stopDF.show()
     stopDF.printSchema()
-    /*
 */
 
     /*
@@ -88,6 +101,7 @@ object TwitterSourceExample {
     val lowerTextDF = enTweetDF
       .withColumn("lowercase",lower(col("text")))
 
+    // The following regular expression take care of removing the punctuation too
     val splitWordsDF = lowerTextDF
       .withColumn("word_list",split(col("lowercase"),"\\W+"))
 
@@ -130,6 +144,9 @@ object TwitterSourceExample {
       .withColumn("neutral", col("positive_count") === col("negative_count"))
 
 
+    // Creates exception:
+    //classPosNegDF.persist()
+
     val win30sSummaryDF = classPosNegDF
       .withWatermark("createdDate","1 minute")
       .groupBy(window(col("createdDate"),"30 seconds"))
@@ -137,8 +154,21 @@ object TwitterSourceExample {
         sum(col("positive").cast("Int")).as("positive_total"),
         sum(col("negative").cast("Int")).as("negative_total"),
         sum(col("neutral").cast("Int")).as("neutral_total"),
-        count(col("text")).as("total")
+        count(col("text")).as("total_30")
       )
+      .select("window.start","window.end","positive_total","negative_total","neutral_total","total_30")
+
+    val win10sSummaryDF = classPosNegDF
+      .withWatermark("createdDate","1 minute")
+      .groupBy(window(col("createdDate"),"10 seconds"))
+      .agg(
+        sum(col("positive").cast("Int")).as("positive_total"),
+        sum(col("negative").cast("Int")).as("negative_total"),
+        sum(col("neutral").cast("Int")).as("neutral_total"),
+        count(col("text")).as("total_10")
+      )
+      .select("window.start","window.end","positive_total","negative_total","neutral_total","total_10")
+
 
     /*
     def udfCheckWord(wordList: Broadcast[Set[String]]): UserDefinedFunction = {
@@ -169,16 +199,48 @@ object TwitterSourceExample {
     //val tweetQS = tweetDF.writeStream.format("console").option("truncate", false).start()
 
     //val tweetQS = wordsSummaryDF.writeStream.format("console").option("truncate", false).start()
-    //val tweetQS = classPosNegDF.writeStream.format("console").option("truncate", false).start()
-    val tweetQS = win30sSummaryDF.writeStream.format("console").option("truncate", false).start()
+
+    if (DEBUG_TWEET_COUNT) {
+      val tweetQS = classPosNegDF.writeStream.format("console").option("truncate", false).start()
+    }
+
+    if (!SINK_REPORT_TO_CVS) {
+      // Console:
+      val tweet30QS = win30sSummaryDF.writeStream.format("console").option("truncate", false).start()
+      val tweet10QS = win10sSummaryDF.writeStream.format("console").option("truncate", false).start()
+      //Thread.sleep(1000 * 35)
+      //Thread.sleep(1000 )
+      tweet30QS.awaitTermination()
+    } else {
+      val outputFolder = "src/main/output"
+      val tweet30QS = win30sSummaryDF
+        .writeStream
+        .format("csv")
+        .option("path", s"$outputFolder/csv30")
+        .option("checkpointLocation", s"$outputFolder/checkpoint30")
+        .start()
+
+      val tweet10QS = win10sSummaryDF
+        .writeStream
+        .format("csv")
+        .option("path", s"$outputFolder/csv10")
+        .option("checkpointLocation", s"$outputFolder/checkpoint10")
+        .start()
+
+      tweet30QS.awaitTermination()
+    }
+
+    /*
+    Doesn't work:
+    18/06/19 09:04:24 ERROR MicroBatchExecution: Query [id = fd15d905-8af5-4d65-8d4a-cb32d9d852eb, runId = b72b3b5d-1b70-4f09-bc00-4887a455b419] terminated with error
+    java.lang.ClassCastException: org.apache.spark.sql.execution.streaming.SerializedOffset cannot be cast to org.apache.spark.sql.sources.v2.reader.streaming.Offset
+    // File:
+    //val tweet10QS = win10sSummaryDF.writeStream.format("console").option("truncate", false).start()
+      */
+
 
     //Exception in thread "main" org.apache.spark.sql.AnalysisException: Multiple streaming aggregations are not supported with streaming DataFrames/Datasets;;
 
 
-
-    //Thread.sleep(1000 * 35)
-    //Thread.sleep(1000 )
-
-    tweetQS.awaitTermination()
   }
 }
