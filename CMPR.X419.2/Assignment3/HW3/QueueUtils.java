c49dcf1488ac91623c85a12ab706acaa267ca1ca
roquealex <roquealex20@hotmail.com>
2019-02-12 15:22:29 -0800

    rename

---
+++ CMPR.X419.2/Assignment3/HW3/QueueUtils.java
@@ -0,0 +1,29 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.stream.IntStream;
+
+public class QueueUtils {
+
+  public static <T> void reverseQueue(Queue<T> q) {
+    T e = q.poll();
+    if (e != null) {
+      reverseQueue(q);
+      q.add(e);
+    }
+  }
+
+  public static void main(String[] s) {
+    LinkedList<Integer> ll= new LinkedList<Integer>();
+    ll.add(1);
+    ll.add(2);
+    ll.add(3);
+    ll.add(4);
+    reverseQueue(ll);
+    for (int i : ll) {
+      System.out.println(i);
+    }
+    System.out.println("reversing");
+  }
+
+}
+
