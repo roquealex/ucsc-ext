5a7da030251e46686452718c419d27e44f00443e
roquealex <roquealex20@hotmail.com>
2019-03-17 18:09:24 -0700

    common words

---
+++ CMPR.X419.2/Assignment7/HW7/CommonDictionary.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.zip.GZIPInputStream;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class CommonDictionary {
+  private HashSet<String> dictionary;
+  private static final String inputFile = "words.txt.gz";
+
+  public CommonDictionary() {
+    dictionary = new HashSet<>(10000);
+    try {
+      InputStream gzipStream = new GZIPInputStream(new FileInputStream(inputFile));
+      BufferedReader reader = new BufferedReader(new InputStreamReader(gzipStream,"UTF-8"));
+
+      String line = null;
+      while ((line = reader.readLine()) != null) {
+        dictionary.add(line.toLowerCase());
+      }
+    } catch (IOException e) {
+      System.err.println(e);
+      System.exit(1);
+    }
+
+    //dictionary.add("cat");
+    //dictionary.add("dog");
+    //dictionary.add("bird");
+  }
+
+  public void show() {
+    Iterator<String> it = dictionary.iterator();
+    while (it.hasNext()) {
+      String str = it.next();
+      System.out.println(str);
+    }
+  }
+
+  public static void main(String s[]) {
+    CommonDictionary dic = new CommonDictionary();
+    dic.show();
+  }
+}
