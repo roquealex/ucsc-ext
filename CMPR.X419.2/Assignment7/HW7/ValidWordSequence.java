143e707be07f459cc961c87d3814599bc735f161
roquealex <roquealex20@hotmail.com>
2019-03-19 12:06:12 -0700

    adding profiler

--- CMPR.X419.2/Assignment7/HW7/ValidWordSequence.java
+++ CMPR.X419.2/Assignment7/HW7/ValidWordSequence.java
@@ -1,13 +1,13 @@
 import java.util.Optional;
 import java.util.function.BiFunction;
+import java.util.ArrayList;
 
 public class ValidWordSequence {
-  // Comparisson method
+  // Comparisson method recursive
   public static boolean isValidRec(String s, Dictionary d) {
     if (d.contains(s)) return true;
     for (int i = 1 ; i < s.length() ; i++) {
       String pre = s.substring(0,i);
-      //System.out.println(pre + " - " + s.substring(i));
       if (d.contains(pre) ) {
         String post = s.substring(i);
         if (isValidRec(post,d)) {
@@ -18,6 +18,7 @@ public class ValidWordSequence {
     return false;
   }
   
+  // Dynamic: Saves in a bidimentional array the solution of the subproblem
   public static boolean isValid(String s, Dictionary d) {
     Boolean results[][] = new Boolean[s.length()][s.length()];
     return isValidDyn(s, d, 0, s.length(),results);
@@ -76,15 +77,17 @@ public class ValidWordSequence {
     return results[0];
   }
 
+  /*
   public static class PerfInfo {
     public PerfInfo(String name) {
       this.name = name;
     }
     public String toCsv() {
-      return String.format("%s,%d,%d,%s",name,size,time,result?"TRUE":"FALSE");
+      return String.format("%s,%d,%d,%d,%s",name,wordSize,setSize,time,result?"TRUE":"FALSE");
     }
     public String name;
-    public int size;
+    public int wordSize;
+    public int setSize;
     public long time;
     public boolean result;
   }
@@ -93,6 +96,7 @@ public class ValidWordSequence {
       BiFunction<String,Dictionary,Boolean> f) {
     return isValidProfiler(s, d, f,null);
   }
+
   public static boolean isValidProfiler(String s, Dictionary d,
       BiFunction<String,Dictionary,Boolean> f, PerfInfo perf) {
     long start = System.nanoTime();
@@ -102,16 +106,19 @@ public class ValidWordSequence {
     if (perf==null) {
       System.out.println("Time elapsed "+timeElapsed);
     } else {
-      perf.size = s.length();
+      perf.wordSize = s.length();
+      perf.wordSize = s.length();
       perf.time = timeElapsed;
       perf.result = result;
     }
     return result;
   }
+  */
 
+  /*
   public static void main(String s[]) {
     int seed = 1;
-    FileDictionary dic = new FileDictionary("words.txt");
+    Dictionary dic = new FileDictionary("English3000.txt");
     dic.show();
     WordRandomizer wr = new WordRandomizer(dic,seed);
 
@@ -120,55 +127,23 @@ public class ValidWordSequence {
     //System.out.println(wr.createRandomMix(10,10));
 
     String str = wr.createRandomValid(1000);
-    //String str = wr.createRandomMix(1000,10);
+    //String str = wr.createRandomMix(3500,10);
 
     System.out.println(str);
-    System.out.println(isValidProfiler(str,dic,ValidWordSequence::isValidRec));
-    System.out.println(isValidProfiler(str,dic,ValidWordSequence::isValid));
-    System.out.println(isValidProfiler(str,dic,ValidWordSequence::isValidSquare));
+
+    ArrayList<BiFunction<String,Dictionary,Boolean>> funcs = new ArrayList<>();
+    funcs.add(ValidWordSequence::isValidRec);
+    funcs.add(ValidWordSequence::isValid);
+    funcs.add(ValidWordSequence::isValidSquare);
+    for(BiFunction<String,Dictionary,Boolean> func : funcs) {
+      System.out.println(isValidProfiler(str,dic,func));
+    }
+    //System.out.println(isValidProfiler(str,dic,ValidWordSequence::isValidRec));
+    //System.out.println(isValidProfiler(str,dic,ValidWordSequence::isValid));
+    //System.out.println(isValidProfiler(str,dic,ValidWordSequence::isValidSquare));
     //System.out.println(isValidSquare(str,dic));
-
-    /*
-    boolean b = isValidRec(
-    "catdogbirdfishwaterwindfiresnowsunmoonlightdarkphoneguitarrazorbookskateshoe",
-    dic);
-
-    boolean b = isValidRec(
-    "moonlighteverlastingxdjdsfalsladhalhdglahdlgjahfdlkjghafldjkghalfjkhgakdfjh",
-    dic);
-    System.out.println(b);
-    System.out.println(b);
-
-    //String str = "snowbunnysun";
-    String str = 
-    //"moonlighteverlastingxdjdsfalsladhalhdglahdlgjahfdlkjghafldjkghalfjkhgakdfjh";
-    "catdogbirdfishwaterwindfiresnowsunmoonlightdarkphoneguitarrazorbookskateshoe";
-
-    b = isValidRec(str,dic);
-    b = isValid(str, dic);
-    b = isValidSquare(str, dic);
-    System.out.println(b);
-    */
   }
+  */
+
 }
 
-/*
-s - nowbunnysun
-sn - owbunnysun
-sno - wbunnysun
-snow - bunnysun
-b - unnysun
-bu - nnysun
-bun - nysun
-bunn - ysun
-bunny - sun
-bunnys - un
-bunnysu - n
-snowb - unnysun
-snowbu - nnysun
-snowbun - nysun
-snowbunn - ysun
-snowbunny - sun
-snowbunnys - un
-snowbunnysu - n
-*/
